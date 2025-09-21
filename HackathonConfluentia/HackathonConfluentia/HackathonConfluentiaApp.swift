fcab6b73ee035c5b6c58f0461f31985123e517b0
mayukhbaidya18 <94429603+mayukhbaidya18@users.noreply.github.com>
2025-09-21 11:32:22 +0530

    Add files via upload
    Initial commit - Hackathon Confluentia

---
+++ HackathonConfluentia/HackathonConfluentia/HackathonConfluentiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonConfluentiaApp.swift
+//  HackathonConfluentia
+//
+//  Created by Mayukh Baidya on 17/09/25.
+//
+
+import SwiftUI
+
+@main
+struct HackathonConfluentiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackathonConfluentia/HackathonConfluentiaTests/HackathonConfluentiaTests.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonConfluentiaTests.swift
+//  HackathonConfluentiaTests
+//
+//  Created by Mayukh Baidya on 17/09/25.
+//
+
+import Testing
+@testable import HackathonConfluentia
+
+struct HackathonConfluentiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
